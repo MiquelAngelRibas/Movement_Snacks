5b4218b20bc462ef7fe835afe19506ca3789a099
Miquel Angel Ribas <miquel@example.com>
2026-08-17 10:22:29 +0200

    fix: optimizar watcher de Vite en Google Drive

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   server: {
     port: 5180,
     watch: {
-      usePolling: true
+      usePolling: true,
+      ignored: ['**/node_modules/**', '**/public/**', '**/dist/**', '**/.git/**']
     }
   }
 })
